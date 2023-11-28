c3be7f0d76ccf639e2a3a338d3ff274645276278
ana.popova <ana.popova@yara.com>
2023-11-28 17:02:15 +0200

    feat(structure): revise functions

--- index.js
+++ index.js
@@ -2,43 +2,40 @@
 //https://www.omdbapi.com/
 //OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=e9f54ad  API KEY
 
+let searchedMoviesData = []; // Array to store data from search
 
 function getMovieByName() {
-  let url = "http://www.omdbapi.com/?s=Nightmare&apikey=e9f54ad";
+  const searchMovie = document.getElementById('searchbar').value;
+  const url = `http://www.omdbapi.com/?s=${searchMovie}&apikey=e9f54ad`;
   fetchMovies(url);
 }
 
-function getMoviesByYear() {
-  let url = "http://www.omdbapi.com/?s=Nightmare&apikey=e9f54ad&type=movie";
-  fetchMovies(url, true);
-}
-
-function fetchMovies(url, sortByYear = false) {
+function fetchMovies(url) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      if (sortByYear) {
-        data.Search.sort((a, b) => b.Year - a.Year); // Sort movies by year
-      }
-      displayMovies(data);
+      searchedMoviesData = data.Search;
+      displayMovies(searchedMoviesData);
     })
     .catch((error) => console.error('Error fetching data:', error));
 }
 
-function displayMovies(movie) {
-  console.log(movie);
-  let movieContainer = document.querySelector('.movie-list');
-  
-  movieContainer.innerHTML = '';// Clear previous content
+function sortMovies(movies) {
+  movies.sort((a, b) => b.Year - a.Year);
+  displayMovies(movies);
+  console.log(movies);
+}
 
-  if (movie.Search && movie.Search.length > 0) {
-    for (let i = 0; i < movie.Search.length; i++) {
-      
+function displayMovies(movies) {
+  console.log(movies);
+  let moviesContainer = document.querySelector('.movie-list');
+  moviesContainer.innerHTML = '';// Clear previous content
+  if (movies && movies.length > 0) {
+    for (let i = 0; i < movies.length; i++) {
       let listItem = document.createElement('li'); // Create a list item for each movie title
       listItem.className = 'movie-list-item';
-      listItem.textContent = `${movie.Search[i].Title} (${movie.Search[i].Year})`;
-
-      movieContainer.appendChild(listItem);// Append the list item to the movie list
+      listItem.textContent = `${movies[i].Title} (${movies[i].Year})`;
+      moviesContainer.appendChild(listItem);// Append the list item to the movie list
     }
   }
 }
